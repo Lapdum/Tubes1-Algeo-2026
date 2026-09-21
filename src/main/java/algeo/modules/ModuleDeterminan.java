1a716b7c51f40d07fe826020ec40cad9f0ac0d85
Cendra Ayam <cendra.asih.c@gmail.com>
2026-09-21 21:11:35 +0700

    fix: progress determinan eskpansibaris dan reduksibaris

--- src/main/java/algeo/modules/ModuleDeterminan.java
+++ src/main/java/algeo/modules/ModuleDeterminan.java
@@ -2,52 +2,37 @@ package algeo.modules;
 
 import java.util.Arrays;
 
-import algeo.modules.Matrix;
-
 public class ModuleDeterminan {
-    Matrix mtrx = new Matrix();
-
-    public void jalanAres() {
-        //System.out.println(Arrays.deepToString(testMatrix));
-        System.out.println(Arrays.deepToString(subMatrix(i, j, m, n, testMatrix)));
-    }
-
-    int[][] testMatrix = { {6, 7, 25, 2}, {2, 9, 11, 1}, {1, 9, 2, 4}, {0, 17, 25, 2} };
-    int m = 4;
-    int n = 4;
-
-    public int ekspansiKofaktorBaris(int x, int n, int[][] testMatrix) {
-        int det = 0;
-        x = x -1;
-        j = 0;
-        while (j < n) {
-            det += testMatrix[x][j]*cofaktor(x, j, m, n, testMatrix);
-            j++;
+    public Double ekspansiKofaktorBaris(int x, Matrix matrix) { //baris ke-n
+        if (matrix.rows != matrix.cols) { return null; }
+        else {
+            x = x-1;
+            double det = 0;
+            for (int c = 0; c < matrix.cols; c++) {
+                det += matrix.data[x][c]*cofaktor(x, c, matrix);
+            }
+            return det;
         }
-        return det;
     }
 
-    static int cofaktor(int i, int j, int m, int n, int[][] testMatrix) {
-        return (-1)^(i+j)*minor(i, j, m, n, testMatrix);
+    public double cofaktor(int i, int j, Matrix matrix) { //cofaktor (Cij)
+        return Math.pow(-1, i+j)*minor(i, j, matrix);
     }
 
-    static int minor(int i, int j, int m, int n, int[][] testMatrix) { 
-        subMatrix(i, j, m, n, testMatrix);
-        if (m > 2) {
-            return ekspansiKofaktorBaris();
+    public double minor(int i, int j, Matrix matrix) { //minor (Mij)
+        subMatrix(i, j, matrix); 
+        if (matrix.cols > 2) {
+            return ekspansiKofaktorBaris(1, matrix);
         } else {
-            return localDeterminan2x2();
+            return localDeterminan2x2(matrix);
         }
     }
 
-    int i = 0;
-    int j = 0;
-
-    static int[][] subMatrix(int i, int j, int m, int n, int[][] testMatrix) {
-        int[][] subM = new int[m-1][n-1];
-        while (i < m-1) {
-            if (j < n-1) {
-                subM[i][j] += testMatrix[i+1][j+1];
+    static Matrix subMatrix(int i, int j, Matrix matrix) {
+        Matrix subM = new Matrix(matrix.rows, matrix.cols);
+        while (i < matrix.rows-1) {
+            if (j < matrix.cols-1) {
+                subM.data[i][j] += matrix.data[i+1][j+1];
                 j++;
             } else {
                 i++;
@@ -57,19 +42,20 @@ public class ModuleDeterminan {
         return subM;
     }
 
-    static int localDeterminan2x2(int[][] testMatrix) {
-        return testMatrix[0][0]*testMatrix[1][1] - testMatrix[0][1]*testMatrix[1][0];
+    static double localDeterminan2x2(Matrix matrix) {
+        return matrix.data[0][0]*matrix.data[1][1] - matrix.data[0][1]*matrix.data[1][0];
     }
 
-    public boolean isSingular(int[][] Matrix) {
-        if (ekspansiKofaktorBaris() == 0) {return true; }
+    public boolean isSingular(Matrix matrix) {
+        if (ekspansiKofaktorBaris(1, matrix) == 0) {return true; }
         else { return false; }
     }
 
-    public double reduksiBaris(int[][] Matrix, int countSwitch){
-        int valueDiagonal = 
-        
-        return (-1)^countSwitch*valueDiagonal;
+    public double reduksiBaris(Matrix matrix, int countSwitch){
+        double valueDiagonal = 0;
+        for(int i = 0; i < matrix.rows; i++){
+            valueDiagonal *= matrix.data[i][i];
+        }
+        return Math.pow(-1, countSwitch)*valueDiagonal;
     }
 }
-    
